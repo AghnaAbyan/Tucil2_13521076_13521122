dadaf56648c6095825b6c83bce523a92d4812ce8
Ulung32 <13521122@mahasiswa.itb.ac.id>
2023-02-28 22:21:15 +0700

    add comment and cleanning code

--- src/Utility.py
+++ src/Utility.py
@@ -24,9 +24,10 @@ def euclidianDistance (Point1, Point2):
 
 def BFSolution (arrayOfPoint):
     shortest = 9999999999999
-    Point1 = []
+    #Point1 dan Point2 adalah 2 poin dengan jarak terpendek
+    Point1 = [] 
     Point2 = []
-    count = 0
+    count = 0 #banyak fungsi euclidianDistance dipanggil
     for i in range(0, len(arrayOfPoint) - 1):
         for j in range(i+1, len(arrayOfPoint)):
             distance = euclidianDistance(arrayOfPoint[i], arrayOfPoint[j])
@@ -38,46 +39,50 @@ def BFSolution (arrayOfPoint):
 
     return shortest, Point1, Point2, count
 
-def partitionOnX(arrayOfPoints, i, j):
-    pivot = arrayOfPoints[(i+j)//2][0]
-    p = i
-    q = j
-    while p < q:
-        while arrayOfPoints[p][0] < pivot:
-            p += 1
-        while arrayOfPoints[q][0] > pivot:
-            q -= 1
-        if p <= q:
-            if p != q:
-                temp = arrayOfPoints[p]
-                arrayOfPoints[p] = arrayOfPoints[q]
-                arrayOfPoints[q] = temp
-            p += 1
-            q -= 1
-    return q
-
-
+# def partitionOnX(arrayOfPoints, i, j):
+#     pivot = arrayOfPoints[(i+j)//2][0]
+#     p = i
+#     q = j
+#     while p < q:
+#         while arrayOfPoints[p][0] < pivot:
+#             p += 1
+#         while arrayOfPoints[q][0] > pivot:
+#             q -= 1
+#         if p <= q:
+#             if p != q:
+#                 temp = arrayOfPoints[p]
+#                 arrayOfPoints[p] = arrayOfPoints[q]
+#                 arrayOfPoints[q] = temp
+#             p += 1
+#             q -= 1
+#     return q
 
 def quicksort(arrayOfPoints):
     if(len(arrayOfPoints) == 0):
+        #jika list tidak memiliki elemen sama sekali maka tidak perlu disorting
         return []
     elif (len(arrayOfPoints) == 1):
+        #jika list hanya memiliki satu elemen tidak perlu disorting
         return arrayOfPoints
     else:
-        pivot = arrayOfPoints[len(arrayOfPoints) // 2][0] # Select the middle element as pivot based on x-value
+        pivot = arrayOfPoints[len(arrayOfPoints) // 2][0] # Ambil elemen yang ada ditengah list sebagai pivot
         left = []
         equal = []
         right = []
 
         for point in arrayOfPoints:
-            if point[0] < pivot:
+            if (point[0] < pivot):
+                #jika nilai x pada point lebih kecil dari pivot, maka point akan dimasukkan ke array left
                 left.append(point)
-            elif point[0] == pivot:
+            elif (point[0] == pivot):
+                #jika nilai x pada point sama dengan pivot, maka point akan dimasukkan ke array equal
                 equal.append(point)
             else:
+                #jika nilai x pada point lebih besar dari pivot, maka point akan dimasukkan ke array besar
                 right.append(point)
 
-        return quicksort(left) + equal + quicksort(right)
+        
+        return quicksort(left) + equal + quicksort(right) #rekursi
 
 def visualize (arrayOfPoint, Point1, Point2):
     if(len(Point1) > 3):
@@ -86,19 +91,23 @@ def visualize (arrayOfPoint, Point1, Point2):
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
         
+        #X,Y,Z adalah list yang digunakan untuk menampung komponen x,y,z dari point
         X = []
         Y = []
         Z = []
         for point in (arrayOfPoint):
             if(point != Point2 and point != Point1):
+                #jika point berbeda dari 2 point terdekat, maka nilai komponen x,y,z akan dimasukan kedalam list
                 X.append(point[0])
                 Y.append(point[1])
                 Z.append(point[2])
         
-        ax.scatter(X, Y, Z, color = "blue")
+        ax.scatter(X, Y, Z, color = "blue") #warnai biru untuk point yang bukan dua point terdekat
 
+        #untuk 2 point terdekat akan diberi warna merah
         ax.scatter(Point1[0], Point1[1], Point1[2], color = "red")
         ax.scatter(Point2[0], Point2[1], Point2[2], color = "red")
+        #memberi garis warna merah yang menghubungkan dua titik terdekat
         ax.plot([Point1[0], Point2[0]], [Point1[1], Point2[1]], [Point1[2], Point2[2]], color = "red")
 
         # Set the axis labels
@@ -109,21 +118,24 @@ def visualize (arrayOfPoint, Point1, Point2):
         # Show the plot
         plt.show()
 
-def closestPairWithDnC (arrayOfPoint, count):
+def closestPairWithDnC (arrayOfPoint):
     if (len(arrayOfPoint) <= 3):
+        #jika ukuran array kurang atau sama dengan 3, maka akan langsung diselesaikan menggunakan bruteforce
         return BFSolution(arrayOfPoint)
     else:
-        count = 0
         mid = len(arrayOfPoint)//2
+        #split array menjadi 2 bagian 
         left = arrayOfPoint[:mid]
         right = arrayOfPoint[mid:]
-        middleX = (left[len(left)-1][0] + right[0][0])/2
-        distanceLeft , Point1L, Point2L, count1 = closestPairWithDnC(left, 0)
-        distanceRight, Point1R, Point2R, count2 = closestPairWithDnC(right, 0)
-        count = count1 + count2
+        middleX = (left[len(left)-1][0] + right[0][0])/2 #koordinat X yang membagi arrayOfPoints menjadi 2
+        #divide
+        distanceLeft , Point1L, Point2L, count1 = closestPairWithDnC(left)
+        distanceRight, Point1R, Point2R, count2 = closestPairWithDnC(right)
+        count = count1 + count2 #banyak operasi perhitungan
         closest = 0
         Point1 = []
         Point2 = []
+        #conquer
         if(distanceLeft > distanceRight):
             closest = distanceRight
             Point1 = Point1R
@@ -136,10 +148,11 @@ def closestPairWithDnC (arrayOfPoint, count):
         inMiddleRange = []
         for i in range(0, len(arrayOfPoint)):
             if(abs(arrayOfPoint[i][0]- middleX) < closest):
+                #mencari titik titik yang berada di range middle - closest sampai middle + closest
                 inMiddleRange.append(arrayOfPoint[i])
         for i in range (0, len(inMiddleRange)-1):
             for j in range(i+1, len(inMiddleRange)):
-                if(abs(inMiddleRange[j][1] - inMiddleRange[i][1]) < closest):
+                if(abs(inMiddleRange[j][1] - inMiddleRange[i][1]) < closest): #jika jarak titik indeks ke j dan i pada sumbu y kurang dari closest, baru dilakukan perhitungan
                     distanceMid = euclidianDistance(inMiddleRange[j], inMiddleRange[i])
                     count +=1
                     if(distanceMid < closest):
--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ BFtime = endBF - startBF
 
 
 startDnC = time.time()
-shortestdnc, Point1dnc, Point2dnc, count2= util.closestPairWithDnC(arrayOfPoints, 0)
+shortestdnc, Point1dnc, Point2dnc, count2= util.closestPairWithDnC(arrayOfPoints)
 endDnC = time.time()
 DnCtime = endDnC - startDnC
 
@@ -32,16 +32,16 @@ print("======================================Brute Force Solution===============
 print("Jarak Terdekat                       : " +str(shortest))
 print("Koordinat titik pertama              : " +str(Point1))
 print("Koordinat titik kedua                : " +str(Point2))
-print("Banyak operasi euclidian distance    : " +str(count1) + " total operasi (spek = Intel Core i7)")
-print("Waktu eksekusi                       : " +str(BFtime) + " detik")
+print("Banyak operasi euclidian distance    : " +str(count1) + " total operasi ")
+print("Waktu eksekusi                       : " +str(BFtime) + " detik (spek = Intel Core i7)")
 
 print(Fore.MAGENTA)
 print("======================================Divide and Conquer Solution======================================")
 print("Jarak Terdekat                       : " +str(shortestdnc))
 print("Koordinat titik pertama              : " +str(Point1dnc))
 print("Koordinat titik kedua                : " +str(Point2dnc))
-print("Banyak operasi euclidian distance    : " +str(count2) + " total operasi (spek = Intel Core i7)")
-print("Waktu eksekusi                       : " +str(DnCtime) + " detik")
+print("Banyak operasi euclidian distance    : " +str(count2) + " total operasi ")
+print("Waktu eksekusi                       : " +str(DnCtime) + " detik (spek = Intel Core i7)")
 
 print(Style.RESET_ALL)
 visualise = input("Ketik (Y/y) jika titik ingin divisualisasikan, ketik tombol lainnya jika tidak!!! ")
